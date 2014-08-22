e0bdf112e866d347708982ba823ea56ae2fa4517
till <till@php.net>
2014-08-22 17:35:39 +0200

    Enhancement: error handling for HTTP

--- src/Http.php
+++ src/Http.php
@@ -37,11 +37,26 @@ class Http
      * @param array  $context
      *
      * @return array
+     * @throws \RuntimeException
      */
     public function request($url, $context)
     {
         $response = file_get_contents($url, false, $this->getContext($context));
-        return json_decode($response, true);
+
+        $matches = array();
+        preg_match('#HTTP/\d+\.\d+ (\d+)#', $http_response_header[0], $matches);
+        if ($matches[1] < 400) {
+            return json_decode($response, true);
+        }
+
+        switch ($matches[1]) {
+            case 401:
+                $msg = "Unauthorized for {$url}!";
+                break;
+            default:
+                $msg = "Error occurred: {$matches[1]}";
+        }
+        throw new \RuntimeException($msg, $matches[1]);
     }
 
     private function getContext($context)
